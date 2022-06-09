c0627800b29ce22d98db7d9035b5dc1392877875
Martin <sisamartin96@gmail.com>
2022-06-09 16:37:32 +0200

    challenge

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,34 @@
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String args[]) {
-        String question = "";
-        String choiceOne = "";
-        String choiceTwo = "";
-        String choiceThree = "";
+        String question = "Who tall is Stephen Curry?";
+        String choiceOne = "179cm";
+        String choiceTwo = "1.95m";
+        String choiceThree = "6foot3inch";
 
-        String correctAnswer = choiceTwo;
+        String correctAnswer = choiceThree;
 
         // Write a print statement asking the question
         // Write a print statement giving the answer choices
+
+        System.out.println(question);
+        System.out.println("Option 1: " + choiceOne);
+        System.out.println("Option 2: " + choiceTwo);
+        System.out.println("Option 3: " + choiceThree);
+        
+        System.out.println("Please enter the your answer:");
+
+        Scanner input = new Scanner(System.in);
+
+        String choice = input.next();
+
+        if(choice.equals(correctAnswer) )
+            System.out.println("Correct!");
+        else    
+            System.out.println("Sorry, the answer is incorrect");
+
 
         // Have the user input an answer
         // Retrieve the user's input
