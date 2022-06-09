3fcc946735b7702b6a77a50d47763a6635446d0a
Martin <sisamartin96@gmail.com>
2022-06-09 15:47:52 +0200

    changed name and added 2 strings

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,12 @@
 public class Main {
 
     public static void main(String[] args) {
+        String studentFirstName = "Martin";
+        String studentLastName = "Sisa";
         int studentAge = 15;
         double studentGPA = 3.45;
-        char studentFirstInitial = 'K';
-        char studentLastInitial = 'H';
+        char studentFirstInitial = studentFirstName.charAt(0);
+        char studentLastInitial = studentLastName.charAt(0);
         boolean hasPerfectAttendance = true;
 
         System.out.println(studentAge);
