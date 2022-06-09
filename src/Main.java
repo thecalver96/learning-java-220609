7d81643403b008709d1e6520d5a0711e35bc8c3a
Martin <sisamartin96@gmail.com>
2022-06-09 16:19:38 +0200

    --m

--- src/Main.java
+++ src/Main.java
@@ -7,5 +7,13 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
 
         int inputtedNum = scanner.nextInt();
+
+        if (inputtedNum < 5 )
+            System.out.println("Enjoy the good luck a friend brings you");
+        else
+            System.out.println("Your shoe selection will make you very happy today");
+    
+        
+    
     }
 }
